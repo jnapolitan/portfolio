cd318901f668ba1f3f9a068a981e7e31f6ac4214
Julian Napolitan <julian.napolitan@gmail.com>
2019-03-07 14:08:49 -0800

    Change image switch logic to use innerHTML for production

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -2,8 +2,8 @@
 
 const header = document.querySelector('#header');
 const headerSpans = header.querySelectorAll('span');
-const profileImage = document.querySelector('.profile-pic');
-const originalImageSrc = "images/profile.png";
+const profileImageDiv = document.querySelector('#profile-pic');
+const originalImageHTML = '<img class="profile-pic" src="images/profile.png" />';
 
 const originalHTML = {
   "firstH1": "Hi, I'm <strong>Julian</strong>.",
@@ -17,23 +17,23 @@ const altHTML = {
   "tagline": "And I've been told by many that I have a pretty good sense of humor."
 };
 
-const altImageSrc = {
-  "firstH1": "images/profile-jules.png",
-  "secondH1": "images/profile-outdoors.png",
-  "tagline": "images/profile-humor.png"
+const altImageHTML = {
+  "firstH1": '<img class="profile-pic" src="images/profile-jules.png" />',
+  "secondH1": '<img class="profile-pic" src="images/profile-outdoors.png" />',
+  "tagline": '<img class="profile-pic" src="images/profile-humor.png" />'
 };
 
 function displayAltHTML() {
   setTimeout(() => {
     this.innerHTML = altHTML[this.id];
-    profileImage.src = altImageSrc[this.id];
+    profileImageDiv.innerHTML = altImageHTML[this.id];
   }, 100);
 }
 
 function displayOriginalHTML() {
   setTimeout(() => {
     this.innerHTML = originalHTML[this.id];
-    profileImage.src = originalImageSrc;
+    profileImageDiv.innerHTML = originalImageHTML;
   }, 100);
 }
 
