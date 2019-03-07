b6d98e903201888646ec9565225db571c9a9f1de
Julian Napolitan <julian.napolitan@gmail.com>
2019-03-06 19:50:44 -0800

    Add personality text hover feature to header

---
+++ assets/js/custom.js
@@ -0,0 +1,31 @@
+// Rollover headers
+
+const header = document.querySelector('#header');
+const headerSpans = header.querySelectorAll('span');
+
+const originalHTML = {
+  "firstH1": "Hi, I'm <strong>Julian</strong>.",
+  "secondH1": "I'm a software engineer.",
+  "tagline": "I love solving problems across the spectrum of software development."
+};
+
+const altHTML = {
+  "firstH1": "Some people call me <strong>Jules</strong>.",
+  "secondH1": "And I also love the outdoors.",
+  "tagline": "And I've been told by many that I have a pretty good sense of humor."
+};
+
+function displayAltHTML() {
+  setTimeout(() => {
+    this.innerHTML = altHTML[this.id];
+  }, 100);
+}
+
+function displayOriginalHTML() {
+  setTimeout(() => {
+    this.innerHTML = originalHTML[this.id];
+  }, 100);
+}
+
+headerSpans.forEach(span => span.addEventListener('mouseenter', displayAltHTML));
+headerSpans.forEach(span => span.addEventListener('mouseleave', displayOriginalHTML));
