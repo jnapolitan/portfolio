cc6f5faf11a4cae371586423ad3c26535fc352a8
Julian Napolitan <julian.napolitan@gmail.com>
2019-03-08 17:16:45 -0800

    Add background tanslate on mousemove

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -28,3 +28,34 @@ function displayOriginalHTML() {
 
 headerSpans.forEach(span => span.addEventListener('mouseenter', displayAltHTML));
 headerSpans.forEach(span => span.addEventListener('mouseleave', displayOriginalHTML));
+
+// Move background
+let lFollowX = 0,
+  lFollowY = 0,
+  x = 0,
+  y = 0,
+  friction = 1 / 30;
+
+function moveBackground() {
+  x += (lFollowX - x) * friction;
+  y += (lFollowY - y) * friction;
+
+  translate = 'translate(' + x + 'px, ' + y + 'px) scale(1.1)';
+
+  const background = document.querySelector('.bg');
+  background.setAttribute(
+    'style', 
+    `-webkit-transorm: ${translate}; -moz-transform: ${translate}; transform: ${translate}`);
+
+  window.requestAnimationFrame(moveBackground);
+}
+
+window.addEventListener('mousemove', e => {
+  console.log(e)
+;  let lMouseX = Math.max(-100, Math.min(100, $(window).width() / 2 - e.clientX));
+  let lMouseY = Math.max(-100, Math.min(100, $(window).height() / 2 - e.clientY));
+  lFollowX = (20 * lMouseX) / 100; // 100 : 12 = lMouxeX : lFollow
+  lFollowY = (10 * lMouseY) / 100;
+});
+
+moveBackground();
